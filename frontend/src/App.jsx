53ee96ef71397135922ef72f5c7d5bd134428423
priyanshu <priyanshurohilla001@gmail.com>
2025-03-08 10:56:50 +0530

    shadcn tailwind working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+    </h1>
+  )
+}
+
+
